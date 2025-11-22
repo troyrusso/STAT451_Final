6bbad6761e1e2d7d455d92882174b7426beabfec
salmonSamH <153345893+salmonSamH@users.noreply.github.com>
2025-11-21 21:53:12 -0800

    edited variable names to make them distinct from others' possible variable names

--- app.R
+++ app.R
@@ -66,38 +66,38 @@ ui <- navbarPage(
       headerPanel("Public transit usage vs. fare costs"),
       column(
         4,
-        radioButtons("country_scatter", "Country",
+        radioButtons("country_scatter_sam", "Country",
                     choices = c("Germany",
                                 setdiff(unique(s_data$country), "Germany")),
                     selected = "Germany"),
-        uiOutput("year_selector_scatter"),
-        checkboxGroupInput("trendline_scatter", "Additional Options",
+        uiOutput("year_selector_scatter_sam"),
+        checkboxGroupInput("trendline_scatter_sam", "Additional Options",
                           choices = "Enable trend line")
       ),
       column(
         8,
-        verbatimTextOutput("plot_hoverinfo"),
-        plotOutput("scatter",
-                  hover = hoverOpts(id = "plot_hover",
+        verbatimTextOutput("plot_hoverinfo_sam"),
+        plotOutput("scatter_sam",
+                  hover = hoverOpts(id = "plot_hover_sam",
                                     delayType = "throttle"),
-                  click = "plot_click"),
-        tableOutput("plot_clickinfo")
+                  click = "plot_click_sam"),
+        tableOutput("plot_clickinfo_sam")
       )
     ),
     fluidRow(
       headerPanel("Public transit usage and fare costs over time"),
       column(
         4,
-        radioButtons("country_line", "Country",
+        radioButtons("country_line_sam", "Country",
                     choices = unique(l_data$country),
                     selected = "Switzerland"),
-        uiOutput("city_selector_line"),
-        uiOutput("year_selector_line")
+        uiOutput("city_selector_line_sam"),
+        uiOutput("year_selector_line_sam")
       ),
       column(
         8,
-        plotOutput("line_trips"),
-        plotOutput("line_costs")
+        plotOutput("line_trips_sam"),
+        plotOutput("line_costs_sam")
       )
     )
   )
@@ -188,41 +188,41 @@ server <- function(input, output) {
     
   })
   
-    output$year_selector_scatter <- renderUI({
-    years <- sort(unique(filter(s_data, country == input$country_scatter)$year))
-    selectInput("year_scatter", "Year", choices = years, selected = 2016)
+  output$year_selector_scatter_sam <- renderUI({
+    years <- sort(unique(filter(s_data, country == input$country_scatter_sam)$year))
+    selectInput("year_scatter_sam", "Year", choices = years, selected = 2016)
   })
-  output$city_selector_line <- renderUI({
-    cities <- sort(unique(filter(l_data, country == input$country_line)$city))
-    selectInput("city_line", "Choose cities",
+  output$city_selector_line_sam <- renderUI({
+    cities <- sort(unique(filter(l_data, country == input$country_line_sam)$city))
+    selectInput("city_line_sam", "Choose cities",
                 choices = cities, multiple = TRUE,
                 selected = c("Zurich", "Geneva", "Basel", "Lausanne", "Bern"))
   })
-  output$year_selector_line <- renderUI({
-    years <- sort(unique(filter(l_data, country == input$country_line)$year))
-    sliderInput("year_line", "Year", min = min(years), max = max(years),
+  output$year_selector_line_sam <- renderUI({
+    years <- sort(unique(filter(l_data, country == input$country_line_sam)$year))
+    sliderInput("year_line_sam", "Year", min = min(years), max = max(years),
                 value = c(min(years), max(years)), step = 1,
                 sep = "", ticks = FALSE)
   })
 
-  highlighted_points <- reactiveVal(NA)
-  printed_points <- reactiveVal(NA)
+  highlighted_points_sam <- reactiveVal(NA)
+  printed_points_sam <- reactiveVal(NA)
 
-  filtered_s_data <- reactive({
-    validate(need(!is.na(input$country_scatter),
+  filtered_s_data_sam <- reactive({
+    validate(need(!is.na(input$country_scatter_sam),
                   message = "Loading...", label = "country"),
-             need(!is.na(input$year_scatter),
+             need(!is.na(input$year_scatter_sam),
                   message = "Loading...", label = "year"))
     s_data %>%
-      filter(country == input$country_scatter) %>%
-      filter(year == input$year_scatter)
+      filter(country == input$country_scatter_sam) %>%
+      filter(year == input$year_scatter_sam)
   })
 
-  observeEvent(input$plot_click, {
-    highlighted_points(
+  observeEvent(input$plot_click_sam, {
+    highlighted_points_sam(
       nearPoints(
-        filtered_s_data(),
-        input$plot_click,
+        filtered_s_data_sam(),
+        input$plot_click_sam,
         "transport_cost",
         "percent_journeys_transport"
       )
@@ -230,11 +230,11 @@ server <- function(input, output) {
   }
   )
 
-  observeEvent(input$plot_hover, {
-    printed_points(
+  observeEvent(input$plot_hover_sam, {
+    printed_points_sam(
       nearPoints(
-        filtered_s_data(),
-        input$plot_hover,
+        filtered_s_data_sam(),
+        input$plot_hover_sam,
         "transport_cost",
         "percent_journeys_transport"
       )
@@ -242,35 +242,35 @@ server <- function(input, output) {
   }
   )
 
-  output$scatter <- renderPlot({
+  output$scatter_sam <- renderPlot({
     # https://www.reddit.com/r/rstats/comments/y8yib8/load_order_of_outputs_in_r_shiny/
     # https://mastering-shiny.org/action-graphics.html#modifying-the-plot
-    scatter_plot <- filtered_s_data() %>%
+    scatter_plot <- filtered_s_data_sam() %>%
       ggplot(mapping = aes(x = transport_cost,
                            y = percent_journeys_transport)) +
       geom_point() +
       labs(title = "Journeys made with public transport vs. transport costs",
-           subtitle = paste0("For cities in ", input$country_scatter,
-                             " in ", input$year_scatter),
+           subtitle = paste0("For cities in ", input$country_scatter_sam,
+                             " in ", input$year_scatter_sam),
            x = paste0("Cost of a combined monthly public transport ticket ",
                       "for 5-10km in the city centre (Euros)"),
            y = "Percentage of journeys made with public transport") +
       theme_bw()
-    if (length(input$trendline_scatter) == 1) {
+    if (length(input$trendline_scatter_sam) == 1) {
       scatter_plot <- scatter_plot +
         geom_smooth(method = "lm")
     }
-    if (all(!is.na(highlighted_points()))) {
+    if (all(!is.na(highlighted_points_sam()))) {
       scatter_plot <- scatter_plot +
-        geom_point(data = highlighted_points() %>%
-                            filter(country == input$country_scatter) %>%
-                            filter(year == input$year_scatter),
+        geom_point(data = highlighted_points_sam() %>%
+                            filter(country == input$country_scatter_sam) %>%
+                            filter(year == input$year_scatter_sam),
                    mapping = aes(x = transport_cost,
                                  y = percent_journeys_transport,
                                  color = city)) +
         labs(title = "Journeys made with public transport vs. transport costs",
-           subtitle = paste0("For cities in ", input$country_scatter,
-                             " in ", input$year_scatter,
+           subtitle = paste0("For cities in ", input$country_scatter_sam,
+                             " in ", input$year_scatter_sam,
                              ". Shaded region corresponds",
                              " to a 95% confidence interval"),
            x = paste0("Cost of a combined monthly public transport ticket ",
@@ -280,37 +280,37 @@ server <- function(input, output) {
     scatter_plot
   })
 
-  output$plot_hoverinfo <- renderText({
-    if (any(!is.na(printed_points())) && nrow(printed_points()) > 0)
-      paste(c("Hovering over:", paste(printed_points()$city, collapse = ", ")))
+  output$plot_hoverinfo_sam <- renderText({
+    if (any(!is.na(printed_points_sam())) && nrow(printed_points_sam()) > 0)
+      paste(c("Hovering over:", paste(printed_points_sam()$city, collapse = ", ")))
     else
       "Hover over or click on a point for more information."
   })
 
-  output$plot_clickinfo <- renderTable({
-    if (any(!is.na(highlighted_points()))) {
-      highlighted_points() %>%
-        filter(country == input$country_scatter) %>%
+  output$plot_clickinfo_sam <- renderTable({
+    if (any(!is.na(highlighted_points_sam()))) {
+      highlighted_points_sam() %>%
+        filter(country == input$country_scatter_sam) %>%
         rename("transport_cost_euros" = "transport_cost")
     } else {
-      highlighted_points()
+      highlighted_points_sam()
     }
   })
 
-  filtered_l_data <- reactive({
-    validate(need(!is.na(input$city_line),
+  filtered_l_data_sam <- reactive({
+    validate(need(!is.na(input$city_line_sam),
                   message = "Please select one or more cities", label = "city"),
-             need(!is.na(input$year_line),
+             need(!is.na(input$year_line_sam),
                   message = "Loading...", label = "year"))
     l_data %>%
-      filter(city %in% input$city_line) %>%
-      filter(year <= max(input$year_line)) %>%
-      filter(year >= min(input$year_line))
+      filter(city %in% input$city_line_sam) %>%
+      filter(year <= max(input$year_line_sam)) %>%
+      filter(year >= min(input$year_line_sam))
   })
 
-  colors <- reactive({
-    if (length(input$city_line) > 0) {
-      l_cities <- filtered_l_data() %>%
+  colors_sam <- reactive({
+    if (length(input$city_line_sam) > 0) {
+      l_cities <- filtered_l_data_sam() %>%
         pull(city) %>%
         unique()
       palette <- hue_pal()(length(l_cities))
@@ -319,52 +319,52 @@ server <- function(input, output) {
     }
   })
 
-  output$line_trips <- renderPlot({
-    sorted_cities <- filtered_l_data() %>%
-      filter(year == max(input$year_line)) %>%
+  output$line_trips_sam <- renderPlot({
+    sorted_cities <- filtered_l_data_sam() %>%
+      filter(year == max(input$year_line_sam)) %>%
       arrange(desc(percent_journeys_transport)) %>%
       pull(city) %>%
       unname() # https://stackoverflow.com/questions/53194184
 
-    filtered_l_data() %>%
+    filtered_l_data_sam() %>%
       ggplot(mapping = aes(x = year, y = percent_journeys_transport,
                            color = city)) +
       geom_point() +
       geom_line(mapping = aes(group = city)) +
       ylim(0, NA) +
       labs(title = "Transit ridership over time",
-          subtitle = paste0(input$country_line, " ", min(input$year_line),
-                            " to ", max(input$year_line)),
+          subtitle = paste0(input$country_line_sam, " ", min(input$year_line_sam),
+                            " to ", max(input$year_line_sam)),
           x = "Year",
           y = "Percentage of journeys made with public transport",
           color = "City") +
       theme_bw() +
-      scale_color_manual(values = colors(), breaks = sorted_cities) +
-      scale_x_continuous(breaks = min(input$year_line):max(input$year_line))
+      scale_color_manual(values = colors_sam(), breaks = sorted_cities) +
+      scale_x_continuous(breaks = min(input$year_line_sam):max(input$year_line_sam))
       # https://ggplot2-book.org/scales-position.html#sec-position-continuous-breaks 
   })
 
-  output$line_costs <- renderPlot({
-    sorted_cities <- filtered_l_data() %>%
-      filter(year == max(input$year_line)) %>%
+  output$line_costs_sam <- renderPlot({
+    sorted_cities <- filtered_l_data_sam() %>%
+      filter(year == max(input$year_line_sam)) %>%
       arrange(desc(transport_cost)) %>%
       pull(city) %>%
       unname() # https://stackoverflow.com/questions/53194184
-    filtered_l_data() %>%
+    filtered_l_data_sam() %>%
       ggplot(mapping = aes(x = year, y = transport_cost,
                            color = city)) +
       geom_point() +
       geom_line(mapping = aes(group = city)) +
       ylim(0, NA) +
       labs(title = "Transport costs over time",
-          subtitle = paste0(input$country_line, " ", min(input$year_line),
-                            " to ", max(input$year_line)),
+          subtitle = paste0(input$country_line_sam, " ", min(input$year_line_sam),
+                            " to ", max(input$year_line_sam)),
           x = "Year",
           y = "Cost of a combined monthly public transport ticket (Euros)",
           color = "City") +
       theme_bw() +
-      scale_color_manual(values = colors(), breaks = sorted_cities) +
-      scale_x_continuous(breaks = min(input$year_line):max(input$year_line))
+      scale_color_manual(values = colors_sam(), breaks = sorted_cities) +
+      scale_x_continuous(breaks = min(input$year_line_sam):max(input$year_line_sam))
   })
 }
 
